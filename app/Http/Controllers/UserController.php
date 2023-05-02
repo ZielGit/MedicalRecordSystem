65436b9aa5f2ecf66324ac81b3d3743b857781eb
Frans <frans_421_12@hotmail.com>
2023-05-01 23:30:13 -0500

    feat: Separando admin y user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,57 +11,8 @@ use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    public function __construct()
+    public function dashboard()
     {
-        $this->middleware('can:users.index')->only('index');
-        $this->middleware('can:users.create')->only('create', 'store');
-        $this->middleware('can:users.edit')->only('edit', 'update');
-        $this->middleware('can:users.show')->only('show');
-        // $this->middleware('can:users.destroy')->only('destroy');
+        return view('front_office.user.dashboard');
     }
-    
-    public function index()
-    {
-        $users = User::all();
-        return view('admin.users.index', compact('users'));
-    }
-
-    public function create()
-    {
-        $roles = Role::all();
-        return view('admin.users.create', compact('roles'));
-    }
-
-    public function store(StoreUserRequest $request)
-    {
-        $user = User::create($request->all());
-        $user->update(['password'=> Hash::make($request->password)]);
-        $user->roles()->sync($request->get('roles'));
-        return redirect()->route('admin.users.index')->with('store', 'ok');
-    }
-
-    public function show(User $user)
-    {
-        return view('admin.users.show', compact('user'));
-    }
-
-    public function edit(User $user)
-    {
-        $roles = Role::get();
-        return view('admin.users.edit', compact('user', 'roles'));
-    }
-
-    public function update(UpdateUserRequest $request, User $user)
-    {
-        $user->update($request->all());
-        $user->roles()->sync($request->roles);
-        return redirect()->route('admin.users.index')->with('update', 'ok');
-    }
-
-    // Quitando el destroy por el momento
-    // public function destroy(User $user)
-    // {
-    //     $user->delete();
-    //     return back();
-    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\RoleController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,15 +18,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front_office.home');
+});
+
+Route::prefix('user')->name('user.')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 });
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
-])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::resource('users', UserController::class)->names('admin.users');
-    Route::resource('roles', RoleController::class)->names('admin.roles');
+])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('users', AdminUserController::class)->names('users');
+    Route::resource('roles', RoleController::class)->names('roles');
 });
